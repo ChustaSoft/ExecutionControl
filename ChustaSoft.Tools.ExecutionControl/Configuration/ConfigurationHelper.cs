2f4df36c5ae93c22386839cc13976591a9b215d1
Xavi Abelaira Rueda <xavi_rueda@outlook.com>
2019-08-13 16:20:02 +0200

    Fixed problem with ExecutionControlContext<Guid> migrations

--- ChustaSoft.Tools.ExecutionControl/Configuration/ConfigurationHelper.cs
+++ ChustaSoft.Tools.ExecutionControl/Configuration/ConfigurationHelper.cs
@@ -18,18 +18,7 @@ namespace ChustaSoft.Tools.ExecutionControl.Configuration
         public static void RegisterExecutionControl<TProcessEnum>(this IServiceCollection services, string connectionString, int minutesToAbort = DEFAULT_ABORT_PROCESS_TIMEOUT)
                 where TProcessEnum : struct, IConvertible
         {
-            services.AddDbContext<ExecutionControlContext>(options => options.UseSqlServer(connectionString));
-
-            services.AddSingleton(new ExecutionControlConfiguration { });
-
-            services.AddTransient<IExecutionRepository<Guid>, ExecutionRepository<Guid>>();
-            services.AddTransient<IExecutionEventRepository<Guid>, ExecutionEventRepository<Guid>>();
-            services.AddTransient<IProcessDefinitionRepository<Guid>, ProcessDefinitionRepository<Guid>>();
-
-            services.AddTransient<IExecutionBusiness<Guid>, ExecutionBusiness<Guid>>();
-            services.AddTransient<IExecutionEventBusiness<Guid>, ExecutionEventBusiness<Guid>>();
-
-            services.AddTransient<IExecutionService<Guid, TProcessEnum>, ExecutionService<Guid, TProcessEnum>>();
+            services.RegisterExecutionControl<Guid, TProcessEnum>(connectionString, minutesToAbort);
         }
 
         public static void RegisterExecutionControl<TKey, TProcessEnum>(this IServiceCollection services, string connectionString, int minutesToAbort = DEFAULT_ABORT_PROCESS_TIMEOUT)
@@ -50,7 +39,7 @@ namespace ChustaSoft.Tools.ExecutionControl.Configuration
             services.AddTransient<IExecutionService<TKey, TProcessEnum>, ExecutionService<TKey, TProcessEnum>>();
         }
 
-        public static void ConfigureExecutionControl(this IApplicationBuilder app, ExecutionControlContext executionControlContext)
+        public static void ConfigureExecutionControl(this IApplicationBuilder app, ExecutionControlContext<Guid> executionControlContext)
         {
             executionControlContext.Database.Migrate();
         }
